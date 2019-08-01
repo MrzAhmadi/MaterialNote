6d60e97ac13ae800eadce3e89ef05f22645e5dae
Mohmmad Reza Ahmadi <smrhmadi12@gmail.com>
2019-08-01 11:59:39 +0430

    add NoteRepository

---
+++ app/src/main/java/com/smrahmadi/materialnote/data/repository/NoteRepository.kt
@@ -0,0 +1,31 @@
+package com.smrahmadi.materialnote.data.repository
+
+import androidx.annotation.WorkerThread
+import com.smrahmadi.materialnote.data.dao.NoteDao
+import com.smrahmadi.materialnote.data.model.Note
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes = noteDao.getAll()
+
+    @WorkerThread
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
+
+    @WorkerThread
+    suspend fun update(note: Note) {
+        noteDao.update(note)
+    }
+
+    @WorkerThread
+    suspend fun delete(note: Note) {
+        noteDao.delete(note)
+    }
+
+    @WorkerThread
+    fun deleteAll() {
+        noteDao.deleteAll()
+    }
+
+}
