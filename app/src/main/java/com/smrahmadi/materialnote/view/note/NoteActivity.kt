52f55e8ae63720b7efcbf06fb9566d8aa1e0c646
Mohammad Reza Ahmadi <smrahmadi12@gmail.com>
2019-08-05 00:38:23 +0430

    Create Note Activity

---
+++ app/src/main/java/com/smrahmadi/materialnote/view/note/NoteActivity.kt
@@ -0,0 +1,14 @@
+package com.smrahmadi.materialnote.view.note
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.smrahmadi.materialnote.R
+
+class NoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
+    }
+}
+
