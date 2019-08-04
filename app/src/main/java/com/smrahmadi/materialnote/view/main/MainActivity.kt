0fcf5df118e6d0a01c911969555e3ba860c45c18
smrahmadi <smrahmadi12@gmail.com>
2019-08-04 17:06:16 +0430

    refactor MainActivity.kt

--- app/src/main/java/com/smrahmadi/materialnote/view/main/MainActivity.kt
+++ app/src/main/java/com/smrahmadi/materialnote/view/main/MainActivity.kt
@@ -36,7 +36,6 @@ class MainActivity : AppCompatActivity() {
 //        setTestData()
     }
 
-
     private fun setTestData(){
         var noteListTempTest: MutableList<Note> = mutableListOf()
         noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
