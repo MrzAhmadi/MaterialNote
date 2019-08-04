f0bb7ea71a6223da2a3bf8c926487688ab7825f8
smrahmadi <smrahmadi12@gmail.com>
2019-08-04 18:34:01 +0430

    add dependency

--- app/src/main/java/com/smrahmadi/materialnote/data/database/NoteDatabase.kt
+++ app/src/main/java/com/smrahmadi/materialnote/data/database/NoteDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.smrahmadi.materialnote.data.dao.NoteDao
 import com.smrahmadi.materialnote.data.model.Note
+import kotlinx.coroutines.runBlocking
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
@@ -13,24 +15,33 @@ abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 
     companion object {
-        @Volatile
         private var INSTANCE: NoteDatabase? = null
 
-        fun getDatabase(context: Context): NoteDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
+        fun getInstance(context: Context): NoteDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
             }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    NoteDatabase::class.java,
-                    "Note_database"
-                ).build()
-                INSTANCE = instance
-                return instance
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, NoteDatabase::class.java, "YourDatabase.db")
+                .addCallback(seedDatabaseCallback(context))
+                .build()
+
+        private fun seedDatabaseCallback(context: Context): Callback {
+            return object : Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    ioThread {
+                        val noteDao = getInstance(context).noteDao()
+                        val note = Note(title = "This is title", description = "This is Description")
+                        runBlocking {
+                            noteDao.insert(note)
+                            noteDao.insert(note)
+                            noteDao.insert(note)
+                        }
+                    }
+                }
             }
         }
     }
-
 }
