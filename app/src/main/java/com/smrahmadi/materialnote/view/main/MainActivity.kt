d1b3c5add893571ba48fc8b0d743c2d3d0e8924a
smrahmadi <smrahmadi12@gmail.com>
2019-08-04 16:55:57 +0430

    declare and implement list in activity

--- app/src/main/java/com/smrahmadi/materialnote/view/main/MainActivity.kt
+++ app/src/main/java/com/smrahmadi/materialnote/view/main/MainActivity.kt
@@ -2,12 +2,35 @@ package com.smrahmadi.materialnote.view.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.smrahmadi.materialnote.R
+import com.smrahmadi.materialnote.utils.NoteListItemDecoration
+import com.smrahmadi.materialnote.view.main.adapter.NoteListAdapter
+import com.smrahmadi.materialnote.viewmodel.NoteViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var liveData: NoteViewModel
+    private lateinit var adapter:NoteListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        liveData = ViewModelProviders.of(this).get(NoteViewModel::class.java)
+        initView()
+        initList()
+    }
+
+    private fun initView() {
         setContentView(R.layout.activity_main)
+        noteList.addItemDecoration(NoteListItemDecoration(resources.getDimension(R.dimen.margin_8dp).toInt()))
+    }
+
+    private fun initList() {
+        adapter = NoteListAdapter()
+        noteList.adapter = adapter
+        liveData.allNotes.observe(this, Observer {
+            adapter.setList(it)
+        })
     }
 }
