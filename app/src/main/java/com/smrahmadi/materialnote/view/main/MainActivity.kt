f1cd340512adbc867b081a5d57fe9f1d4f59b84f
smrahmadi <smrahmadi12@gmail.com>
2019-08-04 17:05:56 +0430

    add listTestData method

--- app/src/main/java/com/smrahmadi/materialnote/view/main/MainActivity.kt
+++ app/src/main/java/com/smrahmadi/materialnote/view/main/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.smrahmadi.materialnote.R
+import com.smrahmadi.materialnote.data.model.Note
 import com.smrahmadi.materialnote.utils.NoteListItemDecoration
 import com.smrahmadi.materialnote.view.main.adapter.NoteListAdapter
 import com.smrahmadi.materialnote.viewmodel.NoteViewModel
@@ -13,7 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var liveData: NoteViewModel
-    private lateinit var adapter:NoteListAdapter
+    private lateinit var adapter: NoteListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         liveData = ViewModelProviders.of(this).get(NoteViewModel::class.java)
@@ -32,5 +33,21 @@ class MainActivity : AppCompatActivity() {
         liveData.allNotes.observe(this, Observer {
             adapter.setList(it)
         })
+//        setTestData()
+    }
+
+
+    private fun setTestData(){
+        var noteListTempTest: MutableList<Note> = mutableListOf()
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        noteListTempTest.add(Note(1,"What is Lorem Ipsum?","is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has "))
+        adapter.setList(noteListTempTest)
     }
 }
