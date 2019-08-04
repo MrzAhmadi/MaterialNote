c6cf1aa3adae8f4ff696018776735046c64e45d1
Mohammad Reza Ahmadi <smrahmadi12@gmail.com>
2019-08-05 00:41:33 +0430

    Create Note Repository

---
+++ app/src/main/java/com/smrahmadi/materialnote/viewmodel/NoteViewModel.kt
@@ -0,0 +1,6 @@
+package com.smrahmadi.materialnote.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class NoteViewModel(application: Application) : AndroidViewModel(application)
