995d817c17c5be94c2f01cb5e767a088adf8745a
Mohammad Reza Ahmadi <smrahmadi12@gmail.com>
2019-08-06 23:29:52 +0430

    implement NoteViewModel

--- app/src/main/java/com/smrahmadi/materialnote/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/smrahmadi/materialnote/viewmodel/NoteViewModel.kt
@@ -2,5 +2,32 @@ package com.smrahmadi.materialnote.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.smrahmadi.materialnote.data.database.NoteDatabase
+import com.smrahmadi.materialnote.data.model.Note
+import com.smrahmadi.materialnote.data.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class NoteViewModel(application: Application) : AndroidViewModel(application)
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: NoteRepository
+
+    init {
+        val noteDao = NoteDatabase.getInstance(application).noteDao()
+        repository = NoteRepository(noteDao)
+    }
+
+    fun insert(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+    fun update(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+
+    fun delete(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+}
