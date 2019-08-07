418d18dda885b0d307bf061c2b1033adc48b26e9
Mohammad Reza Ahmadi <smrahmadi12@gmail.com>
2019-08-08 00:34:55 +0430

    create shoItemOptionsDialog method

--- app/src/main/java/com/smrahmadi/materialnote/App.kt
+++ app/src/main/java/com/smrahmadi/materialnote/App.kt
@@ -5,5 +5,7 @@ import android.app.Application
 class App : Application() {
     companion object {
         const val NOTE_KEY = "note"
+        const val OPEN_OPTION = 0
+        const val DELETE_OPTION = 1
     }
 }
--- app/src/main/java/com/smrahmadi/materialnote/helper/ContextUtils.kt
+++ app/src/main/java/com/smrahmadi/materialnote/helper/ContextUtils.kt
@@ -6,14 +6,31 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.smrahmadi.materialnote.R
 
-fun Context.toast(text:String) {
-    Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+
+fun Context.toast(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
 
-fun Context.showDeleteItemAlert(onPositiveClickListener: DialogInterface.OnClickListener) {
+fun Context.showDeleteItemDialog(onPositiveClickListener: DialogInterface.OnClickListener) {
     val alert = AlertDialog.Builder(this)
     alert.setTitle(getString(R.string.do_you_want_delete_this_item))
     alert.setNegativeButton(resources.getString(R.string.no), null)
     alert.setPositiveButton(resources.getString(R.string.yes), onPositiveClickListener)
     alert.show()
+}
+
+fun Context.shoItemOptionsDialog(callback: ItemsOptionCallback) {
+    val options = arrayOf(
+        resources.getString(R.string.open),
+        resources.getString(R.string.delete)
+    )
+    val alert = AlertDialog.Builder(this)
+    alert.setTitle(resources.getString(R.string.choose_your_action))
+    alert.setItems(options) { _, which ->
+        callback.onDialogOptionClick(which)
+    }
+}
+
+interface ItemsOptionCallback {
+    fun onDialogOptionClick(id: Int)
 }
