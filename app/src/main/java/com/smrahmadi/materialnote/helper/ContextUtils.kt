f4de19d8e2f6288fc5617cbfcc0089b4cf0bb826
Mohammad Reza Ahmadi <smrahmadi12@gmail.com>
2019-08-07 23:49:49 +0430

    create delete alert

--- app/src/main/java/com/smrahmadi/materialnote/helper/ContextUtils.kt
+++ app/src/main/java/com/smrahmadi/materialnote/helper/ContextUtils.kt
@@ -1,8 +1,19 @@
 package com.smrahmadi.materialnote.helper
 
 import android.content.Context
+import android.content.DialogInterface
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.smrahmadi.materialnote.R
 
 fun Context.toast(text:String) {
     Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showDeleteItemAlert(onPositiveClickListener: DialogInterface.OnClickListener) {
+    val alert = AlertDialog.Builder(this)
+    alert.setTitle(getString(R.string.do_you_want_delete_this_item))
+    alert.setNegativeButton(resources.getString(R.string.no), null)
+    alert.setPositiveButton(resources.getString(R.string.yes), onPositiveClickListener)
+    alert.show()
 }
