4d0f1146bd9ed0dfabcda0c05006ff87a448a607
Mohammad Reza Ahmadi <smrahmadi12@gmail.com>
2019-08-08 01:38:31 +0430

    add instance to Application class

--- app/src/main/java/com/smrahmadi/materialnote/App.kt
+++ app/src/main/java/com/smrahmadi/materialnote/App.kt
@@ -1,11 +1,25 @@
 package com.smrahmadi.materialnote
 
 import android.app.Application
+import android.content.Context
 
 class App : Application() {
+
+    init {
+        instance = this
+    }
+
     companion object {
+        private var instance: App? = null
+
+
         const val NOTE_KEY = "note"
         const val OPEN_OPTION = 0
         const val DELETE_OPTION = 1
+
+        fun getContext(): Context {
+            return instance!!.applicationContext
+        }
     }
+
 }
