7b8dd79b40694848e0ce5abb4a940c8ae853b588
Mohammad Reza Ahmadi <smrahmadi12@gmail.com>
2019-08-14 19:36:47 +0430

    refactor NoteListItemDecoration

--- app/src/main/java/com/smrahmadi/materialnote/utils/NoteListItemDecoration.kt
+++ app/src/main/java/com/smrahmadi/materialnote/utils/NoteListItemDecoration.kt
@@ -4,11 +4,10 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteListItemDecoration(val margin:Int) : RecyclerView.ItemDecoration() {
+class NoteListItemDecoration(private val margin: Int) : RecyclerView.ItemDecoration() {
+
     private var position = -1
     private var itemCount = -1
-    
-    
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
